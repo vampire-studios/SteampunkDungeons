ac4ad5d15219d93d52bd224b6330f0b6a68a17ec
Auropol <62548533+Auropol@users.noreply.github.com>
2020-06-14 17:43:36 +0200

    Added tin (without models and textures)
    will be fixed soon !

--- src/main/java/io/github/vampirestudios/steampunk_dungeons/registry/ModBlocks.java
+++ src/main/java/io/github/vampirestudios/steampunk_dungeons/registry/ModBlocks.java
@@ -8,12 +8,16 @@ import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+    static Block LIQUID_TIN;
 
     public static void init() {
         RegistryUtils.register(new Block(AbstractBlock.Settings.copy(Blocks.IRON_ORE)), new Identifier(SteampunkDungeons.MOD_ID, "malachite_ore"));
         RegistryUtils.register(new Block(AbstractBlock.Settings.copy(Blocks.IRON_ORE)), new Identifier(SteampunkDungeons.MOD_ID, "steel_ore"));
         RegistryUtils.register(new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK)), new Identifier(SteampunkDungeons.MOD_ID, "copper_block"));
         RegistryUtils.register(new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK)), new Identifier(SteampunkDungeons.MOD_ID, "steel_block"));
+        RegistryUtils.register(new Block(AbstractBlock.Settings.copy(Blocks.GOLD_BLOCK)), new Identifier(SteampunkDungeons.MOD_ID, "tin_block"));
+        RegistryUtils.register(new Block(AbstractBlock.Settings.copy(Blocks.GOLD_ORE)), new Identifier(SteampunkDungeons.MOD_ID, "tin_ore"));
+        LIQUID_TIN = RegistryUtils.register(new Block(FluidBlock.Settings.copy(Blocks.WATER)), new Identifier(SteampunkDungeons.MOD_ID, "liquid_tin"));
     }
 
 }
