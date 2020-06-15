89e2c2b2c555f306fe497beae4b0c79f0f6d75aa
Auropol <62548533+Auropol@users.noreply.github.com>
2020-06-15 17:59:15 +0200

    Code cleanup baby !
    hopefully will work

--- src/main/java/io/github/vampirestudios/steampunk_dungeons/corrosion/CorrosionPointRegister.java
+++ src/main/java/io/github/vampirestudios/steampunk_dungeons/corrosion/CorrosionPointRegister.java
@@ -1,50 +1,72 @@
 package io.github.vampirestudios.steampunk_dungeons.corrosion;
-
-import io.github.vampirestudios.steampunk_dungeons.registry.ModItems;
-import io.github.vampirestudios.vampirelib.utils.registry.RegistryUtils;
 import net.minecraft.item.Item;
-import net.minecraft.util.Identifier;
+import net.minecraft.util.Tickable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ScheduledTick;
 
 /**
- Main class for the corrosion system. In theory, it should work, but I think we should improve it and work on it.
+ A utility for the corrosion system. The system might require more work on it, but in theory, it could work.
  */
-  public static Item registerItem(Item item, Identifier name, CorrosionPoints corrosionValue) {
-        RegistryUtils.registerItem(item, name);
-        return item;
+public class CorrosionPoints {
+    static ScheduledTick<Integer> getTick;
+    static Tickable tickable;
+    public static int NONE = 0;
+    public static int HALF = 50;
+    public static int FULL = 100;
+    public static BlockPos pos;
+    public static ScheduledTick<Integer> getTickNumber = registerTick(new ScheduledTick<Integer>(pos, 72000));
+    public static ScheduledTick<Integer> tickNumber(ScheduledTick<Integer> getTickNumber) {
+            getTickNumber = CorrosionPoints.getTickNumber;
+            return tickNumber(getTickNumber);
     }
-    public static void convertToCorroded(Item item) {
-        convertToCorroded(ModItems.MECHANICAL_SCRAP); {
-            convertToHalfDamaged();
-        }
-        convertToCorroded(ModItems.HALF_DAMAGED_MECHANICAL_PART); {
-            convertToFullDamaged();
-        }
-        convertToCorroded(ModItems.UNUSABLE_MECHANICAL_PART); {
-            convertToSameValue();
-        }
+    public static ScheduledTick everyHour() {
+       CorrosionPoints.tickNumber(getTickNumber);
+       return everyHour();
 
     }
-    public static Item convertToHalfDamaged() {
-        CorrosionPoints.getCorrosionInHalf(); {
-       replaceItemWith(ModItems.MECHANICAL_PARTS, ModItems.HALF_DAMAGED_MECHANICAL_PART);
+    public static CorrosionPoints getCorrosionInZero() {
+        everyHour(); {
+            registerCorrosionIntForOverflowed(0);
         }
-        return convertToHalfDamaged();
+        return getCorrosionInZero();
+    }
+    public static CorrosionPoints getCorrosionInHalf() {
+        everyHour(); {
+            registerCorrosionIntForHalf(0);
 
+        }
+        return getCorrosionInHalf();
+    }
+    public static CorrosionPoints getCorrosionInFull() {
+        everyHour(); {
+            registerCorrosionIntForFull(0);
+        }
+        return getCorrosionInFull();
+    }
+    public static ScheduledTick<Integer> registerTick(ScheduledTick<Integer> tick) {
+        registerTick(tick);
+        return tick;
 
     }
-    public static Item convertToFullDamaged() {
-        CorrosionPoints.getCorrosionInFull(); {
-        replaceItemWith(ModItems.HALF_DAMAGED_MECHANICAL_PART, ModItems.UNUSABLE_MECHANICAL_PART);
-        }
-        return convertToFullDamaged();
+    public static CorrosionPoints registerCorrosionIntForHalf(Integer startCorrosionPoints) {
+        registerCorrosionIntForHalf(startCorrosionPoints + HALF);
+        return registerCorrosionIntForHalf(startCorrosionPoints + HALF);
     }
-    public static Item convertToSameValue() {
-        CorrosionPoints.getCorrosionInZero(); {
-        return replaceItemWith(ModItems.UNUSABLE_MECHANICAL_PART, ModItems.UNUSABLE_MECHANICAL_PART);
-        }
+    public static Integer registerCorrosionIntForFull(Integer startCorrosionPoints) {
+        registerCorrosionIntForFull(startCorrosionPoints + FULL);
+        return registerCorrosionIntForFull(startCorrosionPoints + FULL);
     }
-    public static Item replaceItemWith(Item item, Item replacingItem) {
-        replaceItemWith(item, replacingItem);
-        return replacingItem;
+    public static Integer registerCorrosionIntForOverflowed(Integer startCorrosionPoints) {
+        registerCorrosionIntForOverflowed(startCorrosionPoints + NONE);
+        return registerCorrosionIntForOverflowed(startCorrosionPoints + NONE);
+    }
+    public static CorrosionPoints withCorrosionForNone(Integer corrosion) {
+        return getCorrosionInHalf();
+    }
+    public static CorrosionPoints withCorrosionForHalf(Integer corrosion) {
+       return getCorrosionInFull();
+    }
+    public static CorrosionPoints withCorrosionForFull(Integer corrosion) {
+     return getCorrosionInZero();
     }
 }
